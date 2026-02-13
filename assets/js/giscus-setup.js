8e2a4bb7a58edbfba1cfbd54ac72886a999df5aa
yuchenwang3 <yuchenwang3@users.noreply.github.com>
2026-02-13 23:52:44 +0000

    deploy: 6d601d1bf98d4da5cf166c7e069e96b2eb84f873

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yuchenwang3/yuchenwang3.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
